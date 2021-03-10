fd622af9cbae1a592aefe5ccdb642c905934c047
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2021-03-10 13:57:34 -0600

    Create subset_reddit.R

---
+++ utilities/subset_reddit.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(lubridate)
+
+
+data <- read_csv("~/reddit_subset_2008.csv")
+
+#firstyear = 1869
+#lastyear = 1874
+
+#data <- data %>%
+#  filter(year(speechdate) > firstyear) %>%
+#  filter(year(speechdate) < lastyear)
+
+data <- data %>%
+  select(body, subreddit)
+
+
+#data <- data %>%
+#  filter(str_detect(body, "\\bhe |\\bshe "))
+
+data <- data %>%
+  filter(str_detect(body, "\\bman |\\bwoman "))
+
+dir <- setwd("~/")
+
+#write_csv(data, file.path(dir, "reddit_2008_he_she.csv"))
+
+write_csv(data, file.path(dir, "reddit_2008_man_woman.csv"))
